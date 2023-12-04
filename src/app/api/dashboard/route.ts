b80f8d238ccc0ea7f0c511f25a9ed56616ff959b
houyuxi <1938913473@qq.com>
2023-12-04 17:15:48 +0800

    feat: modify routes

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,1 +1,7 @@
 //TODO: complete route for dashboard
+import {NextRequest} from "next/server" ;
+
+export const dynamic = "force-dynamic" ; // defaults to force-static
+
+export async function GET(request: NextRequest): Promise<void> {
+}
