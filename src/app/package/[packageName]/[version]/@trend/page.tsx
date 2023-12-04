243236f006ece1a6546ad6adb16f70c85f55be15
houyuxi <1938913473@qq.com>
2023-12-04 17:17:00 +0800

    feat: use route
    fetch data by route handler in page rendering graph of package being dependent amount

--- src/app/package/[packageName]/[version]/@trend/page.tsx
+++ src/app/package/[packageName]/[version]/@trend/page.tsx
@@ -4,6 +4,10 @@ import {JSX} from "react" ;
 import {LineChart} from "@/third-parties/@mui/x-charts" ;
 import {TitledContainer} from "@/components/titled-container/titled-container" ;
 import {PackageNameAndVersion} from "@/types/package" ;
+import {DependencyTree} from "@/types/dependency" ;
+import {NextRequest} from "next/server" ;
+import {TimeUnit} from "@/common/enums/time-unit" ;
+import {StatType} from "@/common/enums/stat-type" ;
 
 function BasicLineChart({x, y}: { x: number[], y: number[] }): JSX.Element {
     return (
@@ -23,12 +27,24 @@ interface TrendProps {
     params: PackageNameAndVersion;
 }
 
-export default function Trend(props: TrendProps): JSX.Element {
+export default async function Trend(props: TrendProps): Promise<JSX.Element> {
     const {
               params: {
                   packageName,
+                  version,
               },
           } = props ;
+
+    const request = new NextRequest(`${process.env.NEXT_PUBLIC_URL}/api/package/${packageName}/${version}/be-dependent-on-amount`) ;
+    const searchParams = request.nextUrl.searchParams ;
+    //TODO: write some interactive components to get the following values
+    searchParams.set("start", "2023-01-01") ;
+    searchParams.set("end", "2023-01-31") ;
+    searchParams.set("timeUnitEnum", TimeUnit.DAY) ;
+    searchParams.set("statTypeEnum", StatType.NPM) ;
+
+    const data: DependencyTree = await fetch(request)
+        .then((res) => res.json()) ;
 
     const fakeX = [1, 2, 3, 5, 8, 10] ;
     const fakeY = [2, 5.5, 2, 8.5, 1.5, 5] ;
