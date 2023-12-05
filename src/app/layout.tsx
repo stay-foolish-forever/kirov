19085943cd1d2f3392f1263be4bf253249992838
houyuxi <1938913473@qq.com>
2023-12-05 09:51:56 +0800

    feat: add robots.txt and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,25 @@ import Appbar from "@/components/appbar/appbar" ;
 const inter = Inter({subsets: ["latin"]}) ;
 
 export const metadata: Metadata = {
-    title      : "Kirov",
-    description: "Dependencies of NPM packages on Github analysis app",
+    title          : "Kirov",
+    description    : "Dependencies of NPM packages on Github analysis app",
+    generator      : "Next.js",
+    applicationName: "kirov",
+    keywords       : ["Next.js", "React", "Typescript", "Cloud Computing"],
+    authors        : [
+        {
+            name: "Hou Yuxi",
+        },
+        {
+            name: "Zengkun",
+        },
+    ],
+    creator        : "Hou Yuxi",
+    formatDetection: {
+        email    : false,
+        address  : false,
+        telephone: false,
+    },
 } ;
 
 export default function RootLayout(
